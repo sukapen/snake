a55bd87aa22f373e37dd72100868bfdf8cad33d9
sukapen <62739638+sukapen@users.noreply.github.com>
2020-03-27 16:14:17 +0700

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    public class Osnova
+    {
+        static List<string[]> frame = new List<string[]>();
+        static bool gameStatus = true;
+        static string hero = "■";
+        static string food = "♥";
+        static string wall = "▄, █, ▀";
+        static string empty = " ";
+        static readonly int x = 20;
+        static readonly int y = 20;
+        public void Map10()
+        {
+            Console.SetWindowSize(x - 4, y - 4);
+            Console.SetBufferSize(x - 4, y - 4);
+            Console.CursorVisible = false;
+            frame.Add(new string[] { " " });
+            frame.Add(new string[] { " ", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", "■", " ", " ", " ", " ", " ", "♥", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀" });
+            frame.Add(new string[] { " " });
+            render();
+            bool vs = false;
+            while (gameStatus)
+            {
+                var KeyInfo = Console.ReadKey();
+                movehero(KeyInfo);
+                render();
+            }
+        }
+        public void Map15()
+        {
+            Console.SetWindowSize(x + 1, y + 1);
+            Console.SetBufferSize(x + 1, y + 1);
+            Console.CursorVisible = false;
+            frame.Add(new string[] { " " });
+            frame.Add(new string[] { " ", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", "■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "♥", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀" });
+            frame.Add(new string[] { " " });
+            render();
+            bool vs = false;
+            while (gameStatus)
+            {
+                var KeyInfo = Console.ReadKey();
+                movehero(KeyInfo);
+                render();
+            }
+        }
+        public void Map20()
+        {
+            Console.SetWindowSize(x + 5, y + 4);
+            Console.SetBufferSize(x + 5, y + 4);
+            Console.CursorVisible = false;
+            frame.Add(new string[] { " " });
+            frame.Add(new string[] { " ", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄", "▄" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", "■", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "♥", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "█", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "█" });
+            frame.Add(new string[] { " ", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", "▀", });
+            frame.Add(new string[] { " " });
+            render();
+            render();
+            bool vs = false;
+            while (gameStatus)
+            {
+                var KeyInfo = Console.ReadKey();
+                movehero(KeyInfo);
+                render();
+            }
+        }
+        public void render()
+        {
+            Console.Clear();
+            for (int i = 0; i < frame.Count; i++)
+            {
+                Console.WriteLine(string.Join("", frame[i]));
+            }
+        }
+        static void Win()
+        {
+            Console.Beep();
+            gameStatus = true;
+            Console.Clear();
+        }
+        public void movehero(ConsoleKeyInfo keyInfo)
+        {
+            for (int i = frame.Count - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < frame[i].Length; j++)
+                {
+                    if (frame[i][j] == hero)
+                    {
+                        if (keyInfo.Key == ConsoleKey.UpArrow)
+                        {
+                            if ((i - 1) >= 0 && frame[i - 1][j] == empty)
+                            {
+                                frame[i][j] = empty;
+                                frame[i - 1][j] = hero;
+                                return;
+                            }
+                            else if ((i - 1) >= 0 && frame[i - 1][j] == food)
+                            {
+                                frame[i][j] = empty;
+                                frame[i - 1][j] = hero;
+                                Win();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.DownArrow)
+                        {
+                            if ((i + 1) <= (frame.Count - 1) && frame[i + 1][j] == empty)
+                            {
+                                frame[i][j] = empty;
+                                frame[i + 1][j] = hero;
+                                return;
+                            }
+                            else if ((i + 1) <= (frame.Count - 1) && frame[i + 1][j] == food)
+                            {
+                                frame[i][j] = empty;
+                                frame[i + 1][j] = hero;
+                                Win();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.LeftArrow)
+                        {
+                            if ((j - 1) >= 0 && frame[i][j - 1] == empty)
+                            {
+                                frame[i][j] = empty;
+                                frame[i][j - 1] = hero;
+                                return;
+                            }
+                            else if ((j - 1) >= 0 && frame[i][j - 1] == food)
+                            {
+                                frame[i][j] = empty;
+                                frame[i][j - 1] = hero;
+                                Win();
+                                return;
+                            }
+                        }
+                        else if (keyInfo.Key == ConsoleKey.RightArrow)
+                        {
+                            if ((j + 1) <= (frame[i].Length - 1) && frame[i][j + 1] == empty)
+                            {
+                                frame[i][j] = empty;
+                                frame[i][j + 1] = hero;
+                                return;
+                            }
+                            else if ((j + 1) <= (frame[i].Length - 1) && frame[i][j + 1] == food)
+                            {
+                                frame[i][j] = empty;
+                                frame[i][j + 1] = hero;
+                                Win();
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Osnova Game = new Osnova();
+            Console.CursorVisible = false;
+            Console.WriteLine("* Добро пожаловать в змейку! *");
+            Console.WriteLine("* Для начала игры выбери сложность игры: *");
+            Console.WriteLine("* 1 - лёгкая, карта 10 на 10 и медленная скорость; *");
+            Console.WriteLine("* 2 - средняя, карта 15 на 15 и нормальная скорость; *");
+            Console.WriteLine("* 3 - сложная, карта 20 на 20 и быстрая скорость; *");
+            Console.WriteLine("* Введите цифру и нажмите Enter для выбора. *");
+            char choise = Convert.ToChar(Console.ReadLine());
+            switch (choise)
+            {
+                case '1':
+                    Console.Clear();
+                    Game.Map10();
+                    Game.render();
+                    break;
+                case '2':
+                    Console.Clear();
+                    Game.Map15();
+                    Game.render();
+                    break;
+                case '3':
+                    Console.Clear();
+                    Game.Map20();
+                    Game.render();
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
