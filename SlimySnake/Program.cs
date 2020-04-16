74be5b3a6f3a35c771dcb94b3f7c6069206e5a98
sukapen <62739638+sukapen@users.noreply.github.com>
2020-04-16 10:02:37 +0700

    Update Program.cs

--- SlimySnake/Program.cs
+++ SlimySnake/Program.cs
@@ -1,4 +1,3 @@
-﻿using Game;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,24 +9,15 @@ namespace SlimySnake
 {
     public class Program
     {
-        //static readonly int x = 69;
-        //static readonly int y = 9;
+        static readonly int x = 69;
+        static readonly int y = 9;
         static void Main(string[] args)
         {
-            //Console.CursorVisible = false;
-            //Console.SetWindowSize(x + 1, y + 1);
-            //Console.SetBufferSize(x + 1, y + 1);
+            Console.CursorVisible = false;
+            Console.SetWindowSize(x + 1, y + 1);
+            Console.SetBufferSize(x + 1, y + 1);
             Menu M = new Menu();
             M.Present();
-            /*Easy E = new Easy();
-            do
-            {
-                E.UpdateMap();
-                E.NewFood();
-                E.ClearMap();
-                E.MoveHero();
-                E.GameOver();
-            } while (E.end);*/
         }
     }
 }
