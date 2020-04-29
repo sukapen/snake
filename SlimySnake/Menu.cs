7ec4b5de5480f606ec6ca0a049079133a1a95a60
sukapen <62739638+sukapen@users.noreply.github.com>
2020-04-30 01:14:08 +0700

    Update Menu.cs

--- SlimySnake/Menu.cs
+++ SlimySnake/Menu.cs
@@ -1,17 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SlimySnake
 {
-    public class Menu 
+    public class Menu
     {
+        // задаём две переменные для изменения окна консоли:
         static readonly int x = 69;
         static readonly int y = 9;
         public void Present()
         {
+            // очищаем консоль:
+            Console.Clear();
+            // задаём цвета консоли и букв:
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Green;
+            // текст в консоли:
             Console.WriteLine(" ");
             Console.WriteLine(" █▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█▄█");
             Console.WriteLine(" █████████████████████████████▀█▀█▀█▀█▀█████████████████████████████");
@@ -21,8 +24,10 @@ namespace SlimySnake
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
             Console.WriteLine(" ███████■ ДЛЯ ПЕРЕХОДА В ГЛАВНОЕ МЕНЮ НАЖМИТЕ ЛЮБУЮ КЛАВИШУ ■███████");
             Console.WriteLine(" █▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█▀█");
+            // задаём параметр и назначаем на буковки перемещение по меню:
             ConsoleKeyInfo Choise = new ConsoleKeyInfo();
             Choise = Console.ReadKey();
+            // в данном случае пользователь может нажать на пробел, выполнится if, если он нажмёт на любую другую кнопку, выполнится else, они одинаковые:
             if (Choise.Key == ConsoleKey.Spacebar)
             {
                 Console.Clear();
@@ -36,6 +41,7 @@ namespace SlimySnake
         }
         public void MainMenu()
         {
+            // задаём размер и буфер консоли (на всякий случай сделал, чтобы меньше глакков было, гы)
             Console.SetWindowSize(x + 1, y + 1);
             Console.SetBufferSize(x + 1, y + 1);
             Console.WriteLine("");
@@ -47,8 +53,10 @@ namespace SlimySnake
             Console.WriteLine(" █████████████████■ ДЛЯ ВЫХОДА ИЗ ИГРЫ НАЖМИТЕ ESC ■████████████████");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
+            // задаём параметр и назначаем на буковки перемещение по меню:
             ConsoleKeyInfo Choise = new ConsoleKeyInfo();
             Choise = Console.ReadKey();
+            // задаём клавиши для перемещения по меню с вызовами функций и очищением консоли:
             if (Choise.Key == ConsoleKey.Q)
             {
                 Console.Clear();
@@ -64,10 +72,12 @@ namespace SlimySnake
                 Console.Clear();
                 Author();
             }
+            // если пользователь нажмёт Escape, то консоль закроется, по сути он выйдет из игры
             else if (Choise.Key == ConsoleKey.Escape)
             {
                 Environment.Exit(0);
             }
+            // так же предусмотрено нажатие на другие клавиши, если случится, издастся звук, якобы "ошибки" и его перебросит на ту же функцию
             else
             {
                 Console.Beep();
@@ -90,8 +100,10 @@ namespace SlimySnake
             Choise = Console.ReadKey();
             if (Choise.Key == ConsoleKey.Q)
             {
+                // чистим консоль, дабы меню пропало, а то достало:
                 Console.Clear();
                 Easy E = new Easy();
+                // задаём цикл дувайл, дабы всё корректно работало:
                 do
                 {
                     Console.ForegroundColor = ConsoleColor.Black;
@@ -130,11 +142,13 @@ namespace SlimySnake
                     H.GameOver();
                 } while (H.end);
             }
+            // возвращает в главное меню:
             else if (Choise.Key == ConsoleKey.Escape)
             {
                 Console.Clear();
                 MainMenu();
             }
+            // так же дабы не было мисскиликов или специально не сломали моё прекрасное меню
             else
             {
                 Console.Beep();
@@ -144,6 +158,7 @@ namespace SlimySnake
         }
         public void Author()
         {
+            // ну тут ясно, автор типо крутой
             Console.WriteLine("");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
@@ -169,6 +184,7 @@ namespace SlimySnake
         }
         public void Upra()
         {
+            // ну это так, чтобы было
             Console.WriteLine("");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
             Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
@@ -192,34 +208,5 @@ namespace SlimySnake
                 Upra();
             }
         }
-        public void GameOver()
-        {
-            Console.SetWindowSize(x + 1, y + 1);
-            Console.SetBufferSize(x + 1, y + 1);
-            Console.WriteLine("");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            Console.WriteLine(" ████████████████████████■    GAME OVER    ■████████████████████████");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            Console.WriteLine(" █████████████■ ДЛЯ ПЕРЕХОДА В ГЛАВНОЕ МЕНЮ НАЖМИТЕ ESC ■███████████");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            Console.WriteLine(" ███████████████████████████████████████████████████████████████████");
-            ConsoleKeyInfo Choise = new ConsoleKeyInfo();
-            Choise = Console.ReadKey();
-            if (Choise.Key == ConsoleKey.Escape)
-            {
-                Console.Clear();
-                MainMenu();
-            }
-            else
-            {
-                Console.Beep();
-                Console.Clear();
-                GameOver();
-            }
-        }
     }
 }
-
-
